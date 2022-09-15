2ce27fccdca3e4cca7b2f90f9f1f79076e98de0a
micahwaweru <micah.waweru@gmail.com>
2022-09-14 19:47:33 -0500

    add log in backend

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const dotenv = require('dotenv');
-const jwt = require('jsonwebtoken');
-
+const routes = require('./routes');
 const sequelize = require('./config/connection');
-const SequelizeStore =require('connect-session-sequelize')(session.Store);
+
+
+
+
+
 
 const app = express();
+let PORT = process.env.PORT || 4001;
+
+
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
+  secret: 'Super secret',
+  cookie:{
+    maxAge: 86400,
+  },
+  resave:false,
+  saveUninitialized:true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
   
 app.use(session(sess));
 app.use(express.json());
@@ -26,45 +35,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 dotenv.config();
 
-let PORT = process.env.PORT || 4001;
-
-
-app.get('/',(req,res)=>{
-    res.send('Hello world!')
-})
+app.use(routes);
 
 
 
-app.post('/user/generateToken',(req,res)=>{
-    let jwtSecretKey=process.env.JWT_SECRET_KEY;
-    let data = {
-        time: Date(),
-        userId: 12,
-    }
 
-    const token = jwt.sign(data,jwtSecretKey)
-
-    res.send(token);
-});
-
-app.get('/user/validateToken',(req,res)=>{
-    let tokenHeaderKey=process.env.TOKEN_HEADER_KEY;
-    let jwtSecretKey = process.env.JWT_SECRET_KEY;
-
-    try{
-        const token = req.header(tokenHeaderKey)
-        const verified =jwt.verify(token,jwtSecretKey);
-        if(verified){
-            return res.send('Token Verified');
-        }
-        else{
-            return res.status(401).send(error)
-        }
-    }
-    catch(error){
-        return res.status(401).send(error)
-    }
-});
 
 
 
